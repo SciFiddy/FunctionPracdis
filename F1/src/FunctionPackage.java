fbd27f0adfbee46202e9ea60222f73bd0b3ce9ae
Brian Abrahamsen <brian.abrahamsen@gmail.com>
2022-04-28 21:16:08 -0700

    Practice from mkyong

--- F1/src/FunctionPackage.java
+++ F1/src/FunctionPackage.java
@@ -33,7 +33,6 @@ public class FunctionPackage {
 
 
 
-
         //
         // Bi-Function
         // https://www.youtube.com/watch?v=VRpHdSFWGPs
@@ -42,11 +41,52 @@ public class FunctionPackage {
         System.out.println("incrementAndMultiply(1,2):=  "  + incrementAndMultiply(1,2));
         System.out.println("incrementAndMultiplyBiFunc(1,2):=  "  + incrementAndMultiplyBiFunc.apply(1,2));
 
+        BiFunction<Integer, Integer,Integer> plus =
+                (operandOne,operandTwo) -> operandOne + operandTwo;
+
+        Integer i = plus.apply(
+                plus.apply(1, 2),
+                plus.apply(1, 2)
+        );
+
+
+        BiFunction<Boolean, Boolean, Boolean> same =
+                (operandOne, operandTwo) -> operandOne.equals(operandTwo);
+
+        Boolean b = same.apply(
+                same.apply(true, true),
+                same.apply(true, true)
+        );
+
+        if (b) {
+            System.out.println(i);
+        } else {
+            System.out.println(-i);
+        }
+
+
+
+
 
         System.out.println("Fizzbin Out!");
+
+
+        String result = powToString(2, 4,
+                (a1, a2) -> Math.pow(a1, a2),
+                (r) -> "Result : " + String.valueOf(r));
+
+        System.out.println(result); // Result : 16.0
+
     }
 
-    //
+    public static <R> R powToString(Integer a1, Integer a2,
+                                    BiFunction<Integer, Integer, Double> func,
+                                    Function<Double, R> func2) {
+
+        return func.andThen(func2).apply(a1, a2);
+
+    }
+
     static int incrementByOneMethod(Integer number) {return  number + 1; }
 
     static Function<Integer, Integer> brokenIncrementFunction = number -> number++;
@@ -55,11 +95,47 @@ public class FunctionPackage {
 
     static Function<Integer, Integer> multiplyByTen = n -> n * 10;
 
-
-    //
     static String incrementAndMultiply(Integer number, Integer multiple) { return Integer.toString((number + 1) * multiple); }
 
     static BiFunction<Integer, Integer,Integer> incrementAndMultiplyBiFunc =
             (number,multiple) -> (number+1) * multiple;
+
+
+
+
+
+
+
+
+
+
+
+
+    /***
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+     */
+
+
+
+
+
+
+
+
+
+
 
 }
